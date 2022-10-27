faccf67867675b048c34c0086eafdce13c6d1e73
gmlove <gracekinglau@gmail.com>
2022-10-27 14:40:42 +0800

    feat: add support for if in check target

--- test/compare-image.js
+++ test/compare-image.js
@@ -5,7 +5,7 @@ looksSame.createDiff(
     reference: "test/code.png",
     current: "test/code.1.png",
     diff: "test/code.diff.png",
-    highlightColor: "#ffffff", // color to highlight the differences
+    highlightColor: "#008000", // color to highlight the differences
     strict: false, // strict comparsion
     tolerance: 2.5,
     antialiasingTolerance: 0,
