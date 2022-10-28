b04ebf5a44f4c453916f3c9d5f01e6e7e60fc21f
gmlove <gracekinglau@gmail.com>
2022-10-28 14:12:17 +0800

    doc: update readme and sample

---
+++ test/sample.sql
@@ -0,0 +1,30 @@
+-- prepare-sql: create table sample.test as select 1 as id, '1' as val
+-- backend: postgres
+-- inputs: sample.test
+-- outputs: a.b
+
+-- 'xtarget' cannot be recognized, should be comment
+-- xtarget=variables
+
+-- 'xx' is not a valid target type, should be illegal word
+-- target=xx
+
+-- target=variables
+select 1 as a, 2 as b
+
+-- target=variables, if=func(a, b, ${cd})
+select 1 as a, 2 as b
+
+-- target=template.abc, if=func(a, b, ${cd?abc}) xx abc
+a=#{right_table}.abc and '#{a}' > 1
+
+-- target=temp.abc
+select * from sample.test where ${abc(right_table=${a})}
+
+-- target=check.func_call(${a}, bc), if=func(a, b, ${cd})
+
+-- target=log.shoule_log_things
+select * from abc limit 2
+
+-- target=output.a.b
+select * from abc
